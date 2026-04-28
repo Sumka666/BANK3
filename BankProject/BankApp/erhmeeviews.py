70e54514b2365c99a4de13941e365ab77f18aff6
Sumka666 <sw23d015@mandakh.edu.mn>
2026-04-28 12:11:22 +0800

    a

--- BankProject/BankApp/erhmeeviews.py
+++ BankProject/BankApp/erhmeeviews.py
@@ -1,30 +1,30 @@
 
 
-from urllib import request
-
 from django.http import JsonResponse
-from httpcore import request
-from streamlit import json
+from django.views.decorators.csrf import csrf_exempt
+import requests, json
 
-from BankProject.BankProject.settings import connectDB, sendResponse, disconnectDB
+from BankProject.settings import connectDB, sendResponse, disconnectDB
 
-
+@csrf_exempt
 def dt_login(request):
     if request.method != "POST":
         data = [{"function": "dt_login"}]
-        return JsonResponse(sendResponse("dt_login", 1001, data))
+        return JsonResponse(sendResponse(request, 1001, data))
 
     try:
         jsons = json.loads(request.body)
     except json.JSONDecodeError:
         data = [{"function": "dt_login"}]
-        return JsonResponse(sendResponse("dt_login", 1002, data))
+        return JsonResponse(sendResponse(request, 1002, data))
 
     required_fields = ["action", "email", "passwordhash"]
     if not all(field in jsons and jsons[field] != "" for field in required_fields):
         data = [{"function": "dt_login"}]
-        return JsonResponse(sendResponse("dt_login", 1003, data))
-
+        return JsonResponse(sendResponse(request, 1003, data))
+    action = jsons["action"]
+    email = jsons["email"]
+    passwordhash = jsons["passwordhash"]
     conn = None
     cur = None
     try:
@@ -32,28 +32,31 @@ def dt_login(request):
         cur = conn.cursor()
 
         sql = """
-            SELECT id, title, content, mood, created_at
-            FROM appback_gratitudeentry
-            ORDER BY id ASC
+            select  account_id, email, firstname, lastname, status, created_at from users
+            where email = %s and passwordhash = %s
         """
-        cur.execute(sql)
+        cur.execute(sql, (email, passwordhash))
         rows = cur.fetchall()
+        if len(rows) != 1:
+            data = []
+            return JsonResponse(sendResponse(request, 1007, data, action))
 
         data = []
         for row in rows:
             data.append({
-                "id": row[0],
-                "title": row[1],
-                "content": row[2],
-                "mood": row[3],
-                "created_at": row[4].strftime('%Y-%m-%d %H:%M:%S') if row[4] else None
+                "account_id": row[0],
+                "email": row[1],
+                "firstname": row[2],
+                "lastname": row[3],
+                "status": row[4],
+                "created_at": row[5].strftime('%Y-%m-%d %H:%M:%S') if row[5] else None
             })
 
-        return JsonResponse(sendResponse("get_entries", 1000, data))
+        return JsonResponse(sendResponse(request, 200, data, action))
 
     except Exception as e:
         data = [{"error": str(e)}]
-        return JsonResponse(sendResponse("get_entries", 1006, data))
+        return JsonResponse(sendResponse(request, 1006, data, action))
 
     finally:
         if cur:
