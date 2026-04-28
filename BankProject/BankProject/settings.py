814c89d3d97d6ef010e5451bfb629719aea2283a
Saihanjargal-cloud0223 <143776166+altka13@users.noreply.github.com>
2026-04-28 11:09:27 +0800

    import

--- BankProject/BankProject/settings.py
+++ BankProject/BankProject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2
+from datetime import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
